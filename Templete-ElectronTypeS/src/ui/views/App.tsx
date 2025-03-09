c2b0998253ba12022bcc84609cc271afccd27442
Transa1 <kevinx.martinez.haro@gmail.com>
2025-03-09 14:54:22 -0700

    Anchor to dashboard

--- Templete-ElectronTypeS/src/ui/views/App.tsx
+++ Templete-ElectronTypeS/src/ui/views/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./../assets/react.svg";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./dashboard/Dashboard";
 import "./../App.css";
 // import "./tailwind.css";
 
@@ -7,26 +9,40 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        Fran Epic deaaaaa Hola Miguel hola estrada hola klep
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div>
+                Fran Epic deaaaaa Hola Miguel hola estrada hola klep
+                <a href="https://react.dev" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+              </div>
+              <h1>Vite + React</h1>
+              <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+                <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+              </div>
+              <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+              </p>
+              <Link to="/dashboard">Go to Dashboard</Link>
+            </>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
