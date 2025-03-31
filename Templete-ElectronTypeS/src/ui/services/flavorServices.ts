ca7a8b97233388e5bb3f050e5c9f16ed3705b505
Gael Borchardt <gaelborchardt@gmail.com>
2025-03-31 12:38:14 -0700

    flavorServices

---
+++ Templete-ElectronTypeS/src/ui/services/flavorServices.ts
@@ -0,0 +1,99 @@
+const API_URL = import.meta.env.ENV_API_URL as string;
+const FLAVOR_API = `${API_URL}/flavors`;
+
+export interface Flavor{
+    id:number;
+    name: string;
+    price: number;
+    image: string;
+}
+
+export interface FlavorList{
+    flavors: Flavor[];
+}
+
+export interface FlavorCreate{
+    name: string;
+    price: number;
+    image: string;
+}
+
+export interface FlavorUpdate{
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+}
+
+
+export async function getFlavors(): Promise<Flavor[]> {
+    const response = await fetch(FLAVOR_API, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+    }
+    const data: FlavorList = await response.json();
+    return data.flavors;
+}
+
+export async function getFlavorById(id: number): Promise<Flavor> {
+    const response = await fetch(`${FLAVOR_API}/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+    }
+    const data: Flavor = await response.json();
+    return data;
+}
+
+
+export async function createFlavor(flavor: FlavorCreate): Promise<Flavor> {
+    const response = await fetch(`${FLAVOR_API}/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(flavor),
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+    }
+    const data: Flavor = await response.json();
+    return data;
+}
+
+
+export async function updateFlavor(flavor: FlavorUpdate): Promise<Flavor> {
+    const response = await fetch(`${FLAVOR_API}/update/${flavor.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(flavor),
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+    }
+    const data: Flavor = await response.json();
+    return data;
+}
+
+export async function deleteFlavor(id: number): Promise<void> {
+    const response = await fetch(`${FLAVOR_API}/delete/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+    }
+}
