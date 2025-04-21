a2f2a27299caaa61fe5d1f82aa308b0cc6f237cd
Jorgedr12 <jorgedrluisj4@gmail.com>
2025-04-20 21:11:43 -0700

    Cajero

--- Templete-ElectronTypeS/src/ui/views/cashier/CashierPending.tsx
+++ Templete-ElectronTypeS/src/ui/views/cashier/CashierPending.tsx
@@ -6,7 +6,7 @@ const CashierPending = () => {
   return (
     <div className='grid gap-4 p-4 grid-cols-[220px,_1fr]'>
       <CashierSidebar />
-      <BodyCashier />
+      <BodyCashier orderStatus='pending' />
     </div>
   )
 }
